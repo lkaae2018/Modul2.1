63a5b69e453b0a02c86a9b05ad4a5ccaf6e0e16c
lkaae2018 <lak@djhhadsten.dk>
2018-12-09 12:21:35 +0100

    modul2.1

--- pmod_install.sh
+++ pmod_install.sh
@@ -1,4 +1,12 @@
+sudo apt update
+sudo apt upgrade
+echo Nu er RPi opdateret!!
+echo
+echo
+echo SPI&VNC skal enables under interfaces!!
 sudo raspi-config
+echo Nu installeres modulerne til PMOD & BME680 
+echo
 
 sudo apt-get install python-pip python-dev libfreetype6-dev libjpeg-dev build-essential
 
