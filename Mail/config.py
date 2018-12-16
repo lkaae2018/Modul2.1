f49c3e55b82b860e22988a22819c5c7ad57e8001
lkaae2018 <lak@djhhadsten.dk>
2018-12-16 11:32:10 +0100

    modul2.1

---
+++ Mail/config.py
@@ -0,0 +1,6 @@
+# Indtast Google mail konto navn nedenfor
+EMAIL_ADDRESS="lkaae15@gmail.com"
+# Indtast password til Googlekonto
+PASSWORD="Miracs0075"
+# Indtast modtages Google mail konto navn nedenfor
+EMAIL_ADDRESS_rx="webmaster@pensionmira.dk"
