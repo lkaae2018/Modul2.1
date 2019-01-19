b94d58262c671de13b728c61306b216ac36316be
lkaae2018 <lak@djhhadsten.dk>
2019-01-19 07:34:28 +0100

    pt72install

--- pt72install.sh
+++ pt72install.sh
@@ -1,3 +1,9 @@
+echo Packet Tracer 7.2 til Linux skal downloads fra følgende adresse:
+echo https://drive.google.com/file/d/1BQNYBCwADYlOpJ4gYA9rTl2U14j5RpUE/view
+echo Tryk på linket og kopier og derefter download filen i biblioteket Hentet!
+echo
+echo Tryk på en tast for at forsætte!
+read m
 mkdir PT72Installer
 sudo tar xvzf 'Packet Tracer 7.2 for Linux 64 bit.tar.gz' -C PT72Installer
 cd PT72Installer
