a1e7e8a3b64fcd6f07af0fbbcf8d8aaee6b1c0b1
lkaae2018 <lak@djhhadsten.dk>
2019-03-18 08:33:15 +0100

    delete

--- DesignSpark.Pmod-master/DesignSpark/Pmod/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2017 RS Components Ltd
-# SPDX-License-Identifier: MIT License
-
-"""
-A Python library for use with a Raspberry Pi, Pmod HAT and selected Pmods
-"""
-
-__version__ = '0.2.0'
--- DesignSpark.Pmod-master/DesignSpark/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright (c) 2017 RS Components Ltd
-# SPDX-License-Identifier: MIT License
-
-from pkgutil import extend_path
-__path__ = extend_path(__path__, __name__)
