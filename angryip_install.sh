25d0de8435760ef6ee70147d07d8e7e5e862eb5a
lkaae2018 <lak@djhhadsten.dk>
2020-04-04 10:27:13 +0200

    Angryip

---
+++ angryip_install.sh
@@ -0,0 +1,7 @@
+echo Nu bliver Angry IP scanner installeret!!
+echo **************************************
+echo **************************************
+sudo gdebi ipscan_3.5.2_amd64.deb 
+sudo wget https://github.com/angryip/ipscan/releases/download/3.5.2/ipscan_3.5.2_amd64.deb
+sudo gdebi ipscan_3.5.2_amd64.deb 
+echo Find Angry IP scanner i program oversigten
--- githubcommit.sh
+++ githubcommit.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #cd linuxopg
-git add *
-git config --global user.email "lak@djhhadsten.dk"
-git config --global user.name "lkaae2018"
+sudo git add *
+sudo git config --global user.email "lak@djhhadsten.dk"
+sudo git config --global user.name "lkaae2018"
 echo Navn til commiten?
 read commit
-git commit -m $commit
-git push
+sudo git commit -m $commit
+sudo git push
