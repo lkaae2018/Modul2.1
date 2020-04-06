09de40616c435cb670a24f27f2f508ab0a897347
lkaae2018 <lak@djhhadsten.dk>
2020-04-06 07:49:09 +0200

    nmap

---
+++ Statemachine_simple/ran.py
@@ -0,0 +1,6 @@
+#Udskriv tilfældige tal
+from random import random
+from time import sleep
+for x in range(10):
+    ran = random()
+    print (x,"tal",ran)
---
+++ nmap.sh
@@ -0,0 +1,6 @@
+echo Installation af nmap IP scanner til Rasp
+
+sudo apt install nmap
+echo
+echo Skriv sudo nmap -sn 192.168.1.0/24 for at scanne
+echo fra 192.168.1.1-254
