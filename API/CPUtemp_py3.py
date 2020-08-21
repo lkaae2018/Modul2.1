3d36a293da9fd2d7f6a7fc6a66aa92a90b4b873f
lkaae2018 <lak@djhhadsten.dk>
2020-08-21 16:59:56 +0200

    Thingspeakcpu

--- API/CPUtemp_py3.py
+++ API/CPUtemp_py3.py
@@ -5,25 +5,26 @@ channel_id = 1049013 # PUT CHANNEL ID HERE
 write_key='B530FR63JBR4S3MN' # PUT YOUR WRITE KEY HERE
 read_key='BQE5HYPZ0YTXJULT' # PUT YOUR READ KEY HERE
 
-def thermometer():
-          
-              
+def thermometer(channel):
+                        
     try:
         #callableculate CPU temperature of Raspberry Pi in Degrees C
         temp = int(open('/sys/class/thermal/thermal_zone0/temp').read()) / 1e3 # Get Raspberry Pi CPU temp
-        params= channel.update({'field5': temp, 'key':key })
+        params= channel.update({'field5': temp})
         print(temp)
                 #print response.status, response.reason
                
-        time.sleep(10)
-                
+        time.sleep(1)
+        read = channel.get({})
+        print(read)
+        
     except:
         print ("connection failed")
         
 if __name__ == "__main__":
-    channel = thingspeak.Channel(id=channel_id, write_key = write_key, api_key = read_key)
+    channel = thingspeak.Channel(id=channel_id, api_key=write_key)
     while True:
-        thermometer()
+        thermometer(channel)
         time.sleep(15)
  
 
