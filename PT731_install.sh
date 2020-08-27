2a48a73299ed9cca63830aa4ae4176d246cfa5cf
lkaae2018 <lak@djhhadsten.dk>
2020-08-27 10:36:07 +0200

    PT731install

---
+++ PT731_install.sh
@@ -0,0 +1,5 @@
+echo Installation af Packet Tracer 7.3.1:
+echo Download: PacketTracer_731_amd64.deb
+sleep(5)
+sudo apt install ./PacketTracer_731_amd64.deb
+echo PT vil være at finde i program oversigten i nedereste ventre hjørne
