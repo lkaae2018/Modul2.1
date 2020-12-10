4cca95a059bc93e4d1cb12f8d13b008096a3f2ae
lkaae2018 <webmaster@pensionmira.dk>
2020-12-10 08:51:18 +0100

    Update Date_file.py
    Date_file ny version

--- Date_file.py
+++ Date_file.py
@@ -1,9 +1,12 @@
 #Kan benyttes i forbindelse med lyskryds, køre over for rødt!!!!!
 import calendar
 import datetime
+from picamera import PiCamera
+
+camera=PiCamera()
 
 d = datetime.today()
 
 date = calender.month_name(d.month) + "_" + str(d.day) + "_" + str(d.year) + "_" + str(d.hour) + "_" + str(d.minute) + "_" + str(d.second)
 
-camera.capture("/home/pi/" + date + ".jpg"
+camera.capture("/home/pi/" + date + ".jpg")
