46d9ceb375ba0ef056e3637da9634e2e53e1b9a5
lkaae2018 <lak@djhhadsten.dk>
2020-12-26 17:25:09 +0100

    Gmail

--- Gmail/Date_file.py
+++ Gmail/Date_file.py
@@ -1,14 +1,12 @@
 #Kan benyttes i forbindelse med lyskryds, køre over for rødt!!!!!
-import Gmail_file
+import Gmail_att
 from datetime import datetime
 
-#from picamera import PiCamera
+from picamera import PiCamera
 
-#camera=PiCamera()
+camera=PiCamera()
 d=datetime.today()
-#d = datetime.datetime.now()
 
-#date=calendar.month_name(d.month)
 print(d)
 #date = calendar.month_name(d.month)
 date = str(d.year) + "_" + str(d.month)+"_"+ str(d.day) + "_" + str(d.hour) + ":" + str(d.minute) + ":" + str(d.second)
---
+++ Gmail/Pic.py
@@ -0,0 +1,8 @@
+import Gmail_att
+import smtplib 
+from email.mime.multipart import MIMEMultipart 
+from email.mime.text import MIMEText 
+from email.mime.base import MIMEBase 
+from email import encoders 
+
+Gmail_att.gmail_file()
