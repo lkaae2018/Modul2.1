bc77cf03f8d581a989e6b7025685abc0ef90afb1
lkaae2018 <lak@djhhadsten.dk>
2020-12-28 12:03:04 +0100

    Gmailfile

--- Gmail/Date_file.py
+++ Gmail/Date_file.py
@@ -1,14 +1,18 @@
 #Kan benyttes i forbindelse med lyskryds, køre over for rødt!!!!!
-import Gmail_att
+import pic_in_mail
 from datetime import datetime
-
+from time import sleep
 from picamera import PiCamera
 
 camera=PiCamera()
-d=datetime.today()
 
-print(d)
-#date = calendar.month_name(d.month)
-date = str(d.year) + "_" + str(d.month)+"_"+ str(d.day) + "_" + str(d.hour) + ":" + str(d.minute) + ":" + str(d.second)
+dato= datetime.now().strftime("%Y_%m_%d-%I_%M_%S_%p")
+print("Dato=",dato)
 
-print(date)
+camera.start_preview
+sleep(2)
+camera.capture("/home/pi/Downloads/lak/modul2.1/Gmail/"+dato+".jpg")
+camera.stop_preview
+dato=dato+".jpg"
+
+pic_in_mail.gmail_file(dato)
